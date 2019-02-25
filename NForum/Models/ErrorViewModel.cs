76f9b1262541506f1f98ca25af71945d1d5978df
Onyekachi Stanley <onyekachila@gmail.com>
2019-02-25 12:12:59 +0100

    Add project files.

---
+++ NForum/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
